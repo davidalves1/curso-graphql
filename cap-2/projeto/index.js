3b7f3c099c2ae0eacb12fbc7f6be9573f68eff3c
davidalves1 <alves.david@outlook.com>
2019-11-21 00:23:05 -0300

    Create relationship

--- cap-2/projeto/index.js
+++ cap-2/projeto/index.js
@@ -3,22 +3,25 @@ const { GraphQLServer } = require("graphql-yoga");
 const users = [
   {
     id: "123132",
+    role_id: 2,
     name: "David Alves",
     email: "alves.david@outlook.com",
     age: 32,
     real_salary: Math.round(Math.random() * 1000000) / 100,
-    vip: true
+    vip: true,
   },
   {
     id: "64654321",
+    role_id: 1,
     name: "José Alves",
     email: "alves.jose@email.com",
     age: 42,
     real_salary: Math.round(Math.random() * 1000000) / 100,
-    vip: false
+    vip: false,
   },
   {
     id: "6543216",
+    role_id: 1,
     name: "João Alves",
     email: "alves.joao@email.com",
     age: 22,
@@ -56,6 +59,7 @@ const typeDefs = `
 
     type User {
       id: ID!
+      role: Role
       name: String!
       email: String!
       age: Int
@@ -80,6 +84,9 @@ const resolvers = {
   User: {
     salary(user) {
       return user.real_salary;
+    },
+    role(user) {
+      return roles.find(role => role.id === user.role_id) || null
     }
   },
   Product: {
